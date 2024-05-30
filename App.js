0964c236cc34c321bbd3349dc5cb6714180fe291
ivethG <ivethglez80@gmail.com>
2024-05-30 14:49:53 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: 'https://farodegracia.com/' }}
+        style={styles.webview}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  webview: {
+    flex: 1,
+  },
+});
